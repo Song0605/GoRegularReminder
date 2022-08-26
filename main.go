f63684007dfe7893a4181f008570bdceb0913ecc
ChenSong <1348117839@qq.com>
2022-08-26 14:06:55 +0800

    ver_0.1

--- main.go
+++ main.go
@@ -12,6 +12,8 @@ import (
 )
 
 func main() {
+	//Modify:2022年8月26日14:05:48 ver 0.1
+	fmt.Println("钉钉喝水提醒小助手 ver_0.1")
 	var timeArr = [...]int{9, 10, 11, 13, 14, 15, 16, 17}
 	var maxNum = len(timeArr) - 1
 	nowNum := -1
@@ -23,6 +25,11 @@ func main() {
 	for num, v := range timeArr {
 		if nowHour <= v {
 			nowNum = num
+			nowMinute := time.Now().Minute()
+			if (nowHour < 13 && nowMinute > 0) || (nowHour >= 13 && nowMinute > 30) {
+				nowNum++
+			}
+			break
 		}
 	}
 	if nowNum == -1 {
@@ -43,7 +50,7 @@ func main() {
 	)
 	// frequency = "*/60"
 	if _time < 12 {
-		minute = "00"
+		minute = "0"
 	} else {
 		minute = "30"
 	}
@@ -57,13 +64,20 @@ func main() {
 		now := time.Now()
 		fmt.Println(now.Hour(), ":", now.Minute())
 		if strconv.Itoa(now.Minute()) == minute && strconv.Itoa(now.Hour()) == hour {
-			Post()
+			Post(nowNum + 1)
+			fmt.Println("已发送")
 			if nowNum == maxNum {
 				c.Stop()
 			}
 			nowNum++
 			_time = timeArr[nowNum]
-			fmt.Println("_time =", _time)
+			hour = strconv.Itoa(_time)
+			if _time < 12 {
+				minute = "0"
+			} else {
+				minute = "30"
+			}
+			fmt.Println("Next:", hour, minute)
 		}
 	})
 	c.Start()
@@ -71,8 +85,8 @@ func main() {
 	// Post()
 }
 
-func Post() {
-	body := "{\"at\":{\"isAtAll\":false},\"text\":{\"content\":\"喝水喝水喝水\"},\"msgtype\":\"text\"}"
+func Post(Num int) {
+	body := "{\"at\":{\"isAtAll\":false},\"text\":{\"content\":\"是时候了！饮下今天的第" + strconv.Itoa(Num) + "杯水吧！\"},\"msgtype\":\"text\"}"
 	res, err := http.Post("https://oapi.dingtalk.com/robot/send?access_token=c74ed8c3a605f20838dd6278daa43f733a6fa077166c10bb05e80d173ec32613", "application/json;charset=utf-8", bytes.NewBuffer([]byte(body)))
 	if err != nil {
 		fmt.Println("Error!\n", err.Error())
