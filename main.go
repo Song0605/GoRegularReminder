775b9f439e0250aee8c9ecada5adaa151e24e4df
ChenSong <1348117839@qq.com>
2022-08-30 09:49:28 +0800

    ver_0.3

--- main.go
+++ main.go
@@ -12,8 +12,8 @@ import (
 )
 
 func main() {
-	fmt.Println("钉钉喝水提醒小助手 ver_0.2")
-	fmt.Println("最后更新时间:2022年8月26日18:15:28")
+	fmt.Println("钉钉喝水提醒小助手 ver_0.3")
+	fmt.Println("最后更新时间:2022年8月30日09:08:44")
 	weekDay := time.Now().Weekday()
 	if weekDay == 6 || weekDay == 7 {
 		fmt.Println("周末就别加班了啊靓仔！~")
@@ -31,7 +31,7 @@ func main() {
 		if nowHour <= v {
 			nowNum = num
 			nowMinute := time.Now().Minute()
-			if (nowHour < 13 && nowMinute > 0) || (nowHour >= 13 && nowMinute > 30) {
+			if nowHour == v && ((nowHour < 13 && nowMinute > 0) || (nowHour >= 13 && nowMinute > 30)) {
 				nowNum++
 			}
 			break
@@ -43,37 +43,27 @@ func main() {
 	if nowNum > maxNum {
 		return
 	}
-	// if nowNum == maxNum && time.Now().Minute() > 30 {
-	// 	return
-	// }
 	_time := timeArr[nowNum]
-	// fmt.Println("_time =", _time)
 	c := cron.New(cron.WithSeconds())
 	var (
-		// frequency string,
 		minute string
 		hour   string
-		// back string,
-		// space string
 	)
-	// frequency = "*/60"
 	if _time < 12 {
 		minute = "0"
 	} else {
 		minute = "30"
 	}
 	hour = strconv.Itoa(_time)
-	// back = "* * 1-5"
-	// space = " "
-	// spec := frequency + space + minute + space + hour + space + back
 	// spec := "*/1 53 14 * * 1-5" //每周1-5 14:53开始 每隔1秒运行一次
 	fmt.Println("Next:", hour, minute)
 	c.AddFunc("00 * * * * ?", func() {
 		now := time.Now()
-		// fmt.Println(now.Hour(), ":", now.Minute())
-		fmt.Printf(". ")
+		fmt.Println("现在时间：", now.Hour(), ":", now.Minute())
 		if strconv.Itoa(now.Minute()) == minute && strconv.Itoa(now.Hour()) == hour {
-			PostDingDing(nowNum + 1)
+			atAll := "true"
+			text := "诸君，是时候了！饮下今天的第" + strconv.Itoa(nowNum+1) + "杯水吧！"
+			PostDingDing(text, atAll)
 			fmt.Println("已发送")
 			if nowNum == maxNum {
 				c.Stop()
@@ -93,11 +83,10 @@ func main() {
 	select {}
 }
 
-func PostDingDing(Num int) {
+func PostDingDing(text string, atAll string) {
 	fmt.Println()
-	text := "是时候了！饮下今天的第" + strconv.Itoa(Num) + "杯水吧！"
 	fmt.Println(text)
-	body := "{\"at\":{\"isAtAll\":true},\"text\":{\"content\":\"" + text + "\"},\"msgtype\":\"text\"}"
+	body := "{\"at\":{\"isAtAll\":" + atAll + "},\"text\":{\"content\":\"" + text + "\"},\"msgtype\":\"text\"}"
 	res, err := http.Post("https://oapi.dingtalk.com/robot/send?access_token=c74ed8c3a605f20838dd6278daa43f733a6fa077166c10bb05e80d173ec32613", "application/json;charset=utf-8", bytes.NewBuffer([]byte(body)))
 	if err != nil {
 		fmt.Println("Error!\n", err.Error())
@@ -107,5 +96,5 @@ func PostDingDing(Num int) {
 	if err != nil {
 		fmt.Println("Error!\n", err.Error())
 	}
-	fmt.Println(string(content))
+	fmt.Println("接口返回值：", string(content))
 }
